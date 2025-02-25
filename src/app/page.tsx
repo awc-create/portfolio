388332f475ef09d8415129c83b8de314a0c627f3
Adnan Said <adnansaid@Adnans-MacBook-Air.local>
2025-02-25 15:15:34 +0000

    Trigger GitHub Actions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 // src/app/page.tsx
 export default function Home() {
-  return (
-    <main className="flex min-h-screen items-center justify-center">
-      <h1 className="text-3xl font-bold">Welcome to AWC Website</h1>
-    </main>
-  );
-}
+    return (
+      <main className="flex min-h-screen items-center justify-center">
+        <h1 className="text-3xl font-bold">Welcome to AWC Website</h1>
+      </main>
+    );
+  }
+  
