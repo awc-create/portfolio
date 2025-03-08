0b68c6ee3efab68fc37b46487bff19ba081c3fda
Adnan Said <adnansaid@Adnans-MacBook-Air.local>
2025-03-08 03:52:50 +0000

    feat/contact: added contact page and enabled email sending

--- src/components/navbar/BottomNav.tsx
+++ src/components/navbar/BottomNav.tsx
@@ -12,19 +12,19 @@ const BottomNav: React.FC = () => {
   return (
     <div className={styles.bottomNav}>
       <div className="flex flex-row justify-around items-center w-full">
-        <Link href="/" className={styles.navItem}>
+        <Link href="/" className={`${styles.navItem} ${isHomeActive ? styles.active : ""}`}>
           <Icon icon={isHomeActive ? "mingcute:home-5-fill" : "mingcute:home-5-line"} width="32" height="32" />
         </Link>
 
-        <Link href="/projects" className={styles.navItem}>
+        <Link href="/projects" className={`${styles.navItem} ${isProjectsActive ? styles.active : ""}`}>
           <Icon icon={isProjectsActive ? "mdi:code" : "mdi:code"} width="32" height="32" />
         </Link>
 
-        <Link href="/about" className={styles.navItem}>
+        <Link href="/about" className={`${styles.navItem} ${isAboutActive ? styles.active : ""}`}>
           <Icon icon={isAboutActive ? "cib:about-me" : "cib:about-me"} width="32" height="32" />
         </Link>
 
-        <Link href="/book-now" className={styles.navItem}>
+        <Link href="/book-now" className={`${styles.navItem} ${isBookingActive ? styles.active : ""}`}>
           <Icon icon={isBookingActive ? "lucide:book-open-check" : "lucide:book-open-check"} width="32" height="32" />
         </Link>
       </div>
